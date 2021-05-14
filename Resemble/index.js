332781f5228daf9911c7cf44bc7dda6863a075c6
maduran13 <ma.duran13@uniandes.edu.co>
2021-05-14 11:14:06 -0500

    Se agregan todos los js y mirar si funciona

--- Resemble/index.js
+++ Resemble/index.js
@@ -2,6 +2,7 @@ const compareImages = require("resemblejs/compareImages")
 const config = require("./config.json");
 const fs = require('fs');
 const cypress = require("cypress");
+const cypress = require("./cypress/integration/palette.spec.js");
 
 
 const { viewportHeight, viewportWidth, options, url } = config;
